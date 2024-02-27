591be2e7021095ce8138c299a81d1b14f7097350
AndresOnate <andrescamiloquimbayo@gmail.com>
2024-02-27 09:48:35 -0500

    UserRepository

--- src/main/java/com/mcsw/receiptapp/ReceiptAppApplication.java
+++ src/main/java/com/mcsw/receiptapp/ReceiptAppApplication.java
@@ -6,10 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReceiptAppApplication {
-
-	UserRepository userRepository = new UserRepository();
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(ReceiptAppApplication.class, args);
 	}
