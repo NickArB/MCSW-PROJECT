3a5e226df4b8fbe736ce183c4455cfe126fbd6a5
Juan Sánchez <juan.sanchez-pe@mail.escuelaing.edu.co>
2024-02-29 16:42:12 -0500

    Vista historial de pagos realizados

---
+++ src/main/resources/META-INF/resources/js/paymentRecord.js
@@ -0,0 +1,46 @@
+function loadPayments() {
+    $.ajax({
+                type: 'GET',
+                url: '/bills',
+                dataType: 'json',
+                success: function(data) {
+                    buildTable(data); // Construye la tabla con los datos obtenidos
+                },
+                error: function(xhr, status, error) {
+                    console.error('Error al cargar las facturas:', error);
+                }
+            });
+}
+
+
+function buildTable(data) {
+        var table = $('<table>').addClass('table').appendTo('#billsTable');
+        var thead = $('<thead>').appendTo(table);
+        var headerRow = $('<tr>').appendTo(thead);
+        $('<th>').text('ID').appendTo(headerRow);
+        $('<th>').text('Correo de usuario').appendTo(headerRow);
+        $('<th>').text('Empresa').appendTo(headerRow);
+        $('<th>').text('Fecha de facturación').appendTo(headerRow);
+        $('<th>').text('Fecha límite de pago').appendTo(headerRow);
+        $('<th>').text('Valor').appendTo(headerRow);
+        $('<th>').text('Estado de pago').appendTo(headerRow);
+
+        var tbody = $('<tbody>').appendTo(table);
+        $.each(data, function(index, bill) {
+                var fila = '<tr>';
+                fila += '<td>' + bill.id + '</td>';
+                fila += '<td>' + bill.userEmail + '</td>';
+                fila += '<td>' + bill.company + '</td>';
+                fila += '<td>' + bill.billingDate + '</td>';
+                fila += '<td>' + bill.deadLine + '</td>';
+                fila += '<td>' + bill.debt + '</td>';
+                fila += '<td>' + bill.paymentStatus + '</td>';
+                fila += '</tr>';
+                table.find('tbody').append(fila); // Agregar la fila a la tabla
+        });
+
+}
+
+$(document).ready(function() {
+   loadPayments();
+});
